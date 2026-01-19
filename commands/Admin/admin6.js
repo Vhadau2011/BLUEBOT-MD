8302159d0ff836542ca1e7a2ff1d7f1a025350d4
Mudau thendo <vhadaumutuku@gmail.com>
2026-01-19 20:57:31 +0200

    Delete commands/Admin/admin6.js

--- commands/Admin/admin6.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'admin6',
-    description: 'Functional Admin command: admin6',
-    category: 'Admin',
-    async execute(sock, msg, args) {
-        await sock.sendMessage(msg.key.remoteJid, { text: 'Admin action admin6 executed successfully.' });
-    }
-};
